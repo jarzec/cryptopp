6820f46a710799d40c9801868814ece08118684b
Jarek Glowacki <jarek.glowacki@gmail.com>
2022-01-27 06:09:55 -0500

    Enable use of Clang for MSVC > 1930

--- config_os.h
+++ config_os.h
@@ -29,7 +29,7 @@
 // https://www.cryptopp.com/wiki/Release_Process#Self_Tests
 // The problems with Clang pretending to be other compilers is
 // discussed at http://github.com/weidai11/cryptopp/issues/147.
-#if (defined(_MSC_VER) && defined(__clang__) && \
+#if (defined(_MSC_VER) && _MSC_VER < 1930 && defined(__clang__) && \
    !(defined( __clang_analyzer__)) && !defined(__INTEL_LLVM_COMPILER))
 # error: "Unsupported configuration"
 #endif
